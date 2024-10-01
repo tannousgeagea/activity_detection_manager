4c199d7ea47bce857e035f7c826eb8b62c261f6c
tannousgeagea <tannous.geagea@wasteant.com>
2024-10-01 10:50:13 +0200

    check imperted tasks

--- activity_detection_manager/events/main.py
+++ activity_detection_manager/events/main.py
@@ -1,28 +1,8 @@
 
-# from common_utils.apis.base import BaseAPI
-# from common_utils.state_machine.core import StateMachine
-
-
-
-# events = [
-#     {'depth': 4.5, 'brightness': 60, 'motion': 0.2},
-#     {'depth': 3.5, 'brightness': 35, 'motion': 56},
-#     {'depth': 1.8, 'brightness': 0.3, 'motion': 0.1},
-# ]
-
-
-# state_machine = StateMachine()
-
-# for event in events:
-#     state = state_machine.handle_event(event)
-#     print(f"Current state: {state}")
-
-
 from events.config.celery_utils import create_celery
-from events.tasks.fetch_api.core import fetch_data
+from events.tasks.fetch_api.core import fetch_data, query_apis
 
 
 celery = create_celery()
-
 celery.autodiscover_tasks(['events.tasks'])
     
